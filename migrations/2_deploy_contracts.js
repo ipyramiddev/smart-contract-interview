1d7843b191ba92b36dc7a55167c5eb4f9d9f66d7
foysol <foysolmiah34@gmail.com>
2022-07-20 11:59:17 +0100

    Adding multicall2 contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,2 +1,3 @@
+// module.exports = require('./2_deploy_helper_contracts')(artifacts, web3);
 // module.exports = require('./2_deploy_non_upgradeable_contracts')(artifacts, web3);
 module.exports = require('./2_deploy_upgradeable_contracts')(artifacts, web3);
